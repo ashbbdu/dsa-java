a2bcb9e4d53117e6ba65d74c5c4a4fcbe0ace65b
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-07-01 23:05:41 +0530

    started dsa with java

---
+++ src/Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 7;
+        int c = a / b;
+        System.out.println(c);
+        double d = (double)a /b;
+        System.out.println(d);
+        System.out.println(12 == 2);
+    }
+}
